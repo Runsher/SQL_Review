bbc677be00b17fe1ffb314371e61077c3c396ead
root <root@db_test-168-22-2252.(none)>
2014-04-17 18:50:09 +0800

    0417pm

--- controllers/Rule.py
+++ controllers/Rule.py
@@ -3,27 +3,51 @@ import tornado.web
 import tornado.ioloop
 import re
 import sys
+import RulePart
 
 reload(sys)
 sys.setdefaultencoding("utf8")
 
 info = []
+ruleList = []
 
 class Rule(tornado.web.RequestHandler):
         def get(self):
-		a,b=[],[]
+		tb_dict = {}
+		engine,collation,key_status,com_status,key_type=['None'],['None'],['0'],['0'],['0']
 		try:
-			a = self.request.arguments["engine"]
+			engine = self.request.arguments["engine"]
 		except Exception,ex:
 			print Exception,ex	
 		try:
-			b = self.request.arguments["key_status"]
+			collation = self.request.arguments["collation"]
+		except Exception,ex:
+			print Exception,ex	
+		try:
+			com_status = self.request.arguments["com_status"]
 		except Exception,ex:
                         print Exception,ex
-		print a,b
-		self.render('rule.html',title='rule',items=info)
+		try:
+			key_status = self.request.arguments["key_status"]
+		except Exception,ex:
+                        print Exception,ex
+		try:
+			key_type = self.request.arguments["key_type"]
+		except Exception,ex:
+                        print Exception,ex
+		#print engine,collation,key_status,com_status,key_type
+		try:
+			tb_dict = dict((['engine',engine[0]],['collation',collation[0]],['com_status',com_status[0]],['key_status',key_status[0]],['key_type',key_type[0]]))
+		except Exception,ex:
+			print Exception,ex
+		rule_name = 'rule'
+		RulePart.RulePart().handlerRules(rule_name,tb_dict)
+		ruleNames = RulePart.RulePart().getRules()
+		print ruleNames
+		self.render('rule.html',title='rule',items=info,ruleList=ruleNames)
 
 
         def post(self):
-		info = ['a','b','c']
-		self.render('rule.html',title='rule',items=info)
+		pass;
+#		info = ['a','b','c']
+#		self.render('rule.html',title='rule',items=info)
